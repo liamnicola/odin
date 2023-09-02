4ae30655ba37c6de858bd2e4ec26ec24d4408bb7
liamnicola <liamnicola@hotmail.com>
2023-09-02 14:52:09 +0100

    display and delete books completed

--- objectsAndConstructors/book.js
+++ objectsAndConstructors/book.js
@@ -1,28 +1,61 @@
-const myLibrary = ["hi"];
+const myLibrary = [];
 
 class Book {
-    constructor(title, author, pages) {
+    constructor(title, author, pages, status) {
         this.title = title;
         this.author = author;
         this.pages = pages;
-        //this.status = status;
+        this.status = status;
     }
 }
 
+document.getElementById("form").addEventListener("click", function(event){
+    event.preventDefault()
+  });
 function addBookToLibrary(){
-    let book = new Book(title, author, pages)
-    myLibrary.push(book)
+    let title = document.getElementById("title").value
+
+    let author = document.getElementById("author").value
+
+    let pages = document.getElementById("pages").value
+
+    let status = document.getElementById("status").value
+
+
+    let addedBook = new Book(title, author, pages, status)
+    myLibrary.push(addedBook)
+    console.log(myLibrary)
+    displayBooks()
 }
 
-function displayBooks(arr){
-    let books = "";
-    for(let i = 0; i < arr.length; i++){
-        books += `<li>${arr[i]}</li>`
+
+function displayBooks(){
+    let library = document.getElementById("listBooks");
+    library.innerHTML="";
+    for(let i = 0; i < myLibrary.length; i++){
+        let book = myLibrary[i]
+        let bookDiv = document.createElement("div");
+        bookDiv.innerHTML = `<div class="Heading">
+        <h3 class="title">${book.title}</h3>
+        <h4 class="author">${book.author}</h4>
+    </div>
+    <div class="bookBody">
+        <p>${book.pages}</p>
+        <p>${book.status}</p>
+        <button onclick="deleteBook(${i})">Delete Book</button>
+    </div>`
+        library.appendChild(bookDiv)
     }
-    return books;
+}
+displayBooks()
+
+
+function deleteBook(id){
+    myLibrary.splice(id, 1)
+    displayBooks();
+    
 }
 
-document.getElementById("listBooks").innerHTML = `<ol>${displayBooks(myLibrary)}</ol>`
 
 function hide(){
     var div = document.getElementById("form")
