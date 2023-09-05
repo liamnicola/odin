08f8886e0fc5cfce6233f598840d690f3f276cf9
liamnicola <liamnicola@hotmail.com>
2023-09-05 12:28:34 +0100

    edit books complete

--- objectsAndConstructors/book.js
+++ objectsAndConstructors/book.js
@@ -2,10 +2,11 @@ const myLibrary = [];
 const colours = ["green", "red", "blue", "purple", "pink"];
 
 class Book {
-    constructor(title, author, pages, completed) {
+    constructor(title, author, pages, pagesRead, completed) {
         this.title = title;
         this.author = author;
         this.pages = pages;
+        this.pagesRead = pagesRead
         this.completed = completed;
     }
 }
@@ -26,10 +27,10 @@ function addBookToLibrary(){
 
     let pages = document.getElementById("pages").value
 
-    let completed = document.getElementById("completed").checked
+    let pagesRead = document.getElementById("pagesRead").value
 
 
-    let addedBook = new Book(title, author, pages, completed)
+    let addedBook = new Book(title, author, pages, pagesRead)
     myLibrary.push(addedBook)
     console.log(myLibrary)
     displayBooks()
@@ -48,49 +49,56 @@ function displayBooks(){
                     <th class="title">Title</th>
                     <th class="author">Author</th>
                     <th>Pages</th>
-                    <th>Status</th>
+                    <th>Pages Read</th>
+                    <th>Action</th>
                 </tr>
             `
             library.appendChild(heading)
     }
     for(let i = 0; i < myLibrary.length; i++){
         let book = myLibrary[i]
-        let bookDiv = document.createElement("tr");
-        bookDiv.setAttribute("id", `book${i}`);
-        if(myLibrary[i].completed === true){
-            bookDiv.classList.add("bookRead");
+        let bookRow = document.createElement("tr");
+
+        bookRow.setAttribute("id", `book${i}`);
+
+        if(100 * (myLibrary[i].pagesRead / myLibrary[i].pages) >= 75){
+            bookRow.style.backgroundColor = "#84c987";
+            bookRow.style.width = myLibrary[i].pagesRead;
+        } else if(100 * (myLibrary[i].pagesRead / myLibrary[i].pages) <= 25){
+            bookRow.style.backgroundColor = "#c9a584";
+        } else if(myLibrary[i].pagesRead === myLibrary[i].pages){
+            bookRow.style.backgroundColor = "#32be2e";
         } else{
-            bookDiv.classList.add("bookUnread");
+            bookRow.style.backgroundColor = "#bcc984";
         }
-        bookDiv.innerHTML = `
+
+        bookRow.innerHTML = `
 
                 <tr>
-                    <td class="title">${book.title}</td>
-                    <td class="author">${book.author}</td>
-                    <td>${book.pages}</td>
-                    <td>${book.completed ? "Read" : "Unread"}</td>
-                    <td>
-                        <button class="action-btn2" onclick="toggleStatus(${i})">Toggle</button>
-                        <button class="action-btn" onclick="deleteBook(${i})">Remove</button>
-                    </td>
+                        <td>${book.title}</td>
+                        <td>${book.author}</td>
+                        <td>${book.pages}</td>
+                        <td>${book.pagesRead}</td>
+                        <td>
+                            <button class="action-btn2" onclick="edit(${i})">Edit</button>
+                            <button class="action-btn" onclick="deleteBook(${i})">Remove</button>
+                        </td>
                 </tr>
             
             `
-        library.appendChild(bookDiv)
+        library.appendChild(bookRow)
     }
 }
 displayBooks()
 
 
-function toggleStatus(id) {
+/*function toggleStatus(id) {
     myLibrary[id].toggleStatus();
-    let row = document.getElementById(`book${id}`)
-    console.log(row)
-    if(myLibrary[id].completed === true){
-        row.style.backgroundColor = "black"
-    }
     displayBooks();
 }
+*/
+
+
 
 function deleteBook(id){
     myLibrary.splice(id, 1)
@@ -98,6 +106,36 @@ function deleteBook(id){
     
 }
 
+function edit(id){
+    let form = document.getElementById(`book${id}`);
+    form.style.backgroundColor = "grey"
+    form.innerHTML = `
+    <tr>
+        <td><input class="form-input" id="edit-title" value="${myLibrary[id].title}"></td>
+        <td><input class="form-input" id="edit-author" value="${myLibrary[id].author}"></td>
+        <td><input class="form-input" id="edit-pages" value="${myLibrary[id].pages}"></td>
+        <td><input class="form-input" id="edit-read" value="${myLibrary[id].pagesRead}"></td>
+        <td><button class="action-btn2" onclick="update(${id})">Confirm</button></td>
+    </tr>`
+}
+
+function update(id){
+    let title = document.getElementById("edit-title").value
+
+    let author = document.getElementById("edit-author").value
+
+    let pages = document.getElementById("edit-pages").value
+
+    let pagesRead = document.getElementById("edit-read").value
+
+
+    myLibrary[id].title = title
+    myLibrary[id].author = author
+    myLibrary[id].pages = pages
+    myLibrary[id].pagesRead = pagesRead
+    console.log(myLibrary[id])
+    displayBooks()
+}
 
 function hide(){
     var div = document.getElementById("form")
@@ -112,19 +150,3 @@ function hide(){
         button.style.backgroundColor = "#4CAF50"
     }
 }
-
-
-
-/* 
-<div class="Heading">
-                <h1>${i}</h1>
-                <h3 class="title">${book.title}</h3>
-                <h4 class="author">${book.author}</h4>
-            </div>
-            <div class="bookBody">
-            <p>${book.pages}</p>
-            <p>${book.completion ? "Read" : "Unread"}</p>
-            <button onclick="toggleStatus(${i})">Toggle</button>
-            <button onclick="deleteBook(${i})">Delete Book</button>
-            </div>`
-*/
