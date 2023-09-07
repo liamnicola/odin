bf91cc55b6edce88e7a154950f3496af160d567a
liamnicola <liamnicola@hotmail.com>
2023-09-07 16:06:51 +0100

    max moves implemented

--- tic-tac-toe/tic.js
+++ tic-tac-toe/tic.js
@@ -1,7 +1,7 @@
 const gameBoard = (() => {
 
     let gameboard = ["", "", "", "", "", "", "", "", "",]
-
+    let turn = []
     const gameRender = () => {
         let gameboardHtml = "";
         gameboard.forEach((square, i) => {
@@ -14,7 +14,13 @@ const gameBoard = (() => {
         })
     }
 
-    return{gameRender};
+    const update = (square, marker) => {
+        gameboard[square] = marker
+    }
+
+    const fetchGameboard = () => gameboard
+
+    return{gameRender, update};
 
 })();
 
@@ -32,25 +38,48 @@ const Game = (() => {
             createPlayer(document.getElementById("player1").value, "X"),
             createPlayer(document.getElementById("player2").value, "O")
         ]
-        currentPlayer = 1;
+        currentPlayer = 0;
         gameOver = false;
         gameBoard.gameRender();
+        let playerDiv = document.getElementById("player-name");
+        playerDiv.innerHTML = `player ${currentPlayer+1}'s Turn!`;
+    }
+
+    const checkWin = (gameboard, marker) => {
+        const win = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6], 
+        ];
+
+
     }
 
     const playerClick = (event) => {
-        console.log(event.target)
         let clickedDiv = event.target;
-        clickedDiv.innerHTML = players[currentPlayer].marker
-        if(currentPlayer === 0){
-            currentPlayer++
+        let id = clickedDiv.id.slice(7)
+        console.log(clickedDiv.innerHTML)
+        if(clickedDiv.innerHTML !== "X" & clickedDiv.innerHTML !== "O"){
+            clickedDiv.innerHTML = players[currentPlayer].marker
+
+            if(currentPlayer === 0){
+                currentPlayer++
+            } else {
+                currentPlayer--
+            }
+
+            let playerDiv = document.getElementById("player-name");
+            playerDiv.innerHTML = `player ${currentPlayer+1}'s Turn!`;
+            gameBoard.update(id, players[currentPlayer].marker)
         } else {
-            currentPlayer--
+            alert("Square has already been selected!")
         }
-
-    }
-
-    console.log(players)
-
+    } 
     return{begin, playerClick};
 
 })();
